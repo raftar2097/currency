9e7610c0c02e61760721f24f362c9c3aefc81a17
Rishab Agarwala <ragarwala2097@gmail.com>
2021-02-24 20:06:09 +0530

    first commit

---
+++ currency.js
@@ -0,0 +1,16 @@
+window.addEventListener("load",()=>{
+    const input = document.querySelector("#input");
+    const output = document.querySelector("#output");
+    const real = document.querySelector("#real");
+    console.log(input);
+    input.addEventListener('input',event => {
+        fetch(`https://api.exchangeratesapi.io/latest?base=USD&symbols=INR`)
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data);
+            const value = data.rates.INR;
+            real.innerHTML = "Current Rate: "+value;
+            output.value = (input.value/value*10).toFixed(2);        
+        })
+    })
+})
